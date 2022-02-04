68077bb72388dd11138ae91a577ee6c4c4db6946
kaydo1506 <okedoifeoma@yahoo.com>
2022-02-04 20:04:48 +0100

    added offset to nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,15 @@ import { Link } from 'react-scroll';
 const Navbar = () => (
     <>
         <div className='nav'>
-            <Link to='profile' activeClass='active' spy={true} smooth={true}>
+            <Link to='profile' activeClass='active' spy={true} smooth={true} offset={-35}>
                 Profile
             </Link>
 
-            <Link to='experience' spy={true} smooth={true}>
+            <Link to='experience' spy={true} smooth={true} offset={-35}>
                 Experience
             </Link>
 
-            <Link to='portfolio' spy={true} smooth={true}>
+            <Link to='portfolio' spy={true} smooth={true} offset={-35}>
                 Portfolio
             </Link>
         </div>
